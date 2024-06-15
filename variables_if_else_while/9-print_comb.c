4e29beafaadb4a659dee6325bf6d74cfc036ed48
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-06-15 13:14:41 +0000

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0' ; a <= '9' ; a++)
+	{
+		putchar(a);
+
+		if (a == '9')
+		{
+			break;
+		}
+	putchar(',');
+		putchar(' ');
+	}
+	putchar('$');
+	return (0);
+}
