86a8246e98022c5daeffe991083b72619718729e
RorigoNovelli <RodrigoNovelli21@hotmail.com>
2024-06-17 15:01:13 +0000

    adding variable description for betty with @

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,13 +4,12 @@
  * _islower - checks if the letter is lower case.
  * Descritption: this is a function knows if your letter is
  * lower or upper case.
+ * @c: This will be the letter that you enter
  * Return: 0 or 1.
  */
 
 int _islower(int c)
 {
-	int c;
-
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
