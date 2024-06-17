0def8029cefb7a82d9d2fcb0ab4a1a2556eb9044
RorigoNovelli <RodrigoNovelli21@hotmail.com>
2024-06-17 20:00:22 +0000

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -11,28 +12,23 @@ void jack_bauer(void)
 	int c;
 	int d;
 
-	for (a = 0 ; a <= 2 ; a++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0 ; b <= 3 ; b++) 
+		for (b = 0; b < 10 && (a == 2 ? b < 4 : 1); b++) 
 		{
-			for (c = 0 ; c <= 5 ; c++)
+			for (c = 0; c <= 5; c++)
 			{
-				for (d = 0 ; d <= 9 ; d++)
+				for (d = 0; d <= 9; d++)
 				{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
 				}
-					if (a == 2 && b == 3 && c == 5 && d == 9)
-					{
-						break;
-					}
 			}
 		}
-
 	}
 
 }
