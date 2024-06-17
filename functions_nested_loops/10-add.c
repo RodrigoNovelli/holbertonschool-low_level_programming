248577610aefad5e3461596f2d7cdf537df55944
Rodrigo Novelli <Rodrigonovelli21@hotmial.com>
2024-06-17 21:42:35 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - This function makes an add with two variables
+ *@a: This is the first number that you choose
+ *@b: This is the second number that you must choose
+ *Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = (a) + (b);
+	return (sum);
+}
