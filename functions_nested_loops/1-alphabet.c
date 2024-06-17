12e13b8bbb508f39a2ed4e6571a4f140a84ae2c3
RorigoNovelli <RodrigoNovelli21@hotmail.com>
2024-06-17 12:48:18 +0000

    making a to z with for loop

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a' ; l <= 'z' ; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+
+	return (0);
+}
