96ada6e56155721acec855ad473e6f777ca94561
RodrigoNovelli <rodrigonovelli21@hotmail.com>
2024-06-18 15:16:24 +0000

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_diagonal - makes a diagonal line
+ *@n: is the number of lines that the diagonal will be
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int b;
+
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (b = 0; b <= i; b++)
+			{
+				if (b == i)
+				{
+					_putchar('\\');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
