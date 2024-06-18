ff332f3a788a0f2c583e3dbd33d4470bc9649546
RodrigoNovelli <rodrigonovelli21@hotmail.com>
2024-06-18 19:35:24 +0000

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 -Prints a secunece of numbers.
@@ -8,81 +9,29 @@
 
 void print_to_98(int n)
 {
-	int a;
-	int b;
-	
-	for (n = n; n < 9; n++)
+	if (n == 98)
 	{
-		_putchar(n + '0');
-		_putchar(',');
-		if (n >= 10)
+		printf("%d\n",n);
+	}
+	else if (n > 98)
+	{
+		if (n == 98)
 		{
-			a = n / 10;
-			b = n % 10;
-			_putchar(a + '0');
-			_putchar(b + '0');
-			_putchar(',');
-			n ++;
+			printf("%d\n",n);
+		}
+		for (n = n; n >= 98; n--)
+			printf("%d, ",n);
+	}
+	else
+	{
+		for (n = n; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d\n",n);
+			}
+			else
+			printf("%d, ",n);
 		}
 	}
 }
-
-
-void print_to_98(int n)
-{
-	int a;
-	int b; 
-	
-	if (n <= 9)
-	  for (n = n; n <= 9; n++) 
-			{
-				_putchar(n);
-				_putchar(',');
-				if (n > 9)
-				{
-				for (n = n; n <= 98; n++)
-				{
-					a = n / 10;
-					b = n % 10;
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(',');
-				}
-				}
-			}
-			if ( n <0 )
-			{
-				for (n = n; n < 0; n++)
-				{ 
-					a = n * -1
-					_putchar('-');
-					_putchar(a + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			if (n >= 100)
-			{
-				c = n / 100;
-				a = (n / 10) % 10;
-				b = n % 10;
-				for ( n = n; n >= 98; n--)
-				_putchar(c + '0');
-				_putchar(a + '0');
-				_putchar(b + '0');
-				_putchar(',');
-				_putchar(',');
-				if (n < 100 && n > 98)
-				{
-					for (n = n; n > 98 && n < 100; n--) 
-					{
-					a = n / 10;
-					b = n % 10;
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(',');
-					}
-				}
-			}
-}
-
