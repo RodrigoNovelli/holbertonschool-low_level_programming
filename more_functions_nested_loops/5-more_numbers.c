8ef6ebcaa4c82455bf812a53683472a276580b49
RodrigoNovelli <rodrigonovelli21@hotmail.com>
2024-06-18 14:32:20 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * more_numbers - check the code
@@ -24,6 +25,6 @@ void more_numbers(void)
 				_putchar(m + '0');
 			}
 		}
-		_putchar('\n');
+		printf("\n");
 	}
 }
