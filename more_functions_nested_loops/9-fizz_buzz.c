bb4db91a79ce5518047184ca7f9724e333e8b1dc
RodrigoNovelli <rodrigonovelli21@hotmail.com>
2024-06-18 16:23:12 +0000

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints all numbers from
+ * 1 - 100.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 3) == 0 && (n % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		printf("%d ", n);
+	}
+}
