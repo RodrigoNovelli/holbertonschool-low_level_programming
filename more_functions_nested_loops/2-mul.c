26ec2004c7b0dc5cc398fa1cf603b166f03ea98c
RodrigoNovelli <rodrigonovelli21@hotmail.com>
2024-06-18 13:26:23 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers.
+ *@a: first factor.
+ *@b: second factor.
+ * Return: Always m.
+ */
+
+int mul(int a, int b)
+{
+	int m; 
+
+	m = a * b;
+	return (m);
+}
