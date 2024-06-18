143ffe8e5ef94085684fe8224c429bd9c3cda873
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-06-17 21:45:27 -0300

    Actualizar 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -26,3 +26,63 @@ void print_to_98(int n)
 		}
 	}
 }
+
+
+void print_to_98(int n)
+{
+	int a;
+	int b; 
+	
+	if (n <= 9)
+	  for (n = n; n <= 9; n++) 
+			{
+				_putchar(n);
+				_putchar(',');
+				if (n > 9)
+				{
+				for (n = n; n <= 98; n++)
+				{
+					a = n / 10;
+					b = n % 10;
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(',');
+				}
+				}
+			}
+			if ( n <0 )
+			{
+				for (n = n; n < 0; n++)
+				{ 
+					a = n * -1
+					_putchar('-');
+					_putchar(a + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			if (n >= 100)
+			{
+				c = n / 100;
+				a = (n / 10) % 10;
+				b = n % 10;
+				for ( n = n; n >= 98; n--)
+				_putchar(c + '0');
+				_putchar(a + '0');
+				_putchar(b + '0');
+				_putchar(',');
+				_putchar(',');
+				if (n < 100 && n > 98)
+				{
+					for (n = n; n > 98 && n < 100; n--) 
+					{
+					a = n / 10;
+					b = n % 10;
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(',');
+					}
+				}
+			}
+}
+
