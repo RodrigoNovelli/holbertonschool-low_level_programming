a041bf32b644a6faacb471267984376e099f7df0
RodrigoNovelli <rodrigonovelli21@hotmail.com>
2024-06-18 15:45:15 +0000

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int l;
+	int a;
+
+	if (size == 0 || size < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (l = 1; l <= size; l++)
+		{
+			for (a = 1; a <= size; a++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
