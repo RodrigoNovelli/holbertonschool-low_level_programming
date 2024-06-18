e4741b3ce288e615bd2e968680be5a947fe3d3de
RodrigoNovelli <rodrigonovelli21@hotmail.com>
2024-06-18 14:44:05 +0000

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - print a line .
+ *@n: th enumber of _
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n == 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		else
+		_putchar('_');
+	}
+	_putchar('\n');
+}
