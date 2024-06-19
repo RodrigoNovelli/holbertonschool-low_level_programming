eeb1a17b9509221c6cea233379ddc21fc44a4a76
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-06-19 17:12:00 +0000

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - you can change the value of a variable:
+ * @*n: The pointer of the var you wanna change.
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
