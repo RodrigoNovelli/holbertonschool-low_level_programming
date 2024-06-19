2a680aeeaaad7ad69766ad298b4d2b053ef5efa7
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-06-19 18:47:21 +0000

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,27 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ *_strlen - counts the ch of a string
+ *@s: the string you wanna count
+ * Return: the amount of characters
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	return (counter);
+}
+
+/**
+ * _puts - prints the string
+ *@str: the string you wanna print
+ * Return: void
  */
 
 void _puts(char *str)
@@ -12,9 +30,9 @@ void _puts(char *str)
 	int j;
 
 	j = _strlen(str);
-	 for (c = 0; c < j; c++)
-	 {
-		 _putchar(str[c]);
-	 }
-	 _putchar('\n');
+	for (c = 0; c < j; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
 }
