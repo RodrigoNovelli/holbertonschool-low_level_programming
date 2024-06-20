27a461b563dba5e97dc657d64a8546a095ef58bc
Rodrigo Novelli <rodrigonovelli21@hotmail.com>
2024-06-20 12:46:59 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *_strlen - counts the ch of a string
+ *@s: the string you wanna count
+ * Return: the amount of characters
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	return (counter);
+}
+
+/**
+ * print_rev - prints the string in reverse
+ *@s: the string you wanna print
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int l;
+	int counter;
+
+	counter = _strlen(s);
+	for (l = counter; l >= 0; l--)
+	{
+		_putchar(s[l]);
+	}
+	_putchar('\n');
+}
