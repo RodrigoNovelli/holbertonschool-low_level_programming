509885f2c844ee51b0d6ac60a5fc3f29d8efa16b
Rodrigo Novelli <rodrigonovelli21@hotmail.com>
2024-06-20 16:01:12 +0000

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *_strlen - counts the ch of a string
+ *@s: the string you wanna count
+ * Return: the amount of characters
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	return (counter);
+}
+
+/**
+ * puts_half - prints the last half of a string
+ *@str: The string you wanna reverse
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int largo = _strlen(str);
+	int half = largo / 2;
+	int l;
+
+
+	for (l = half; l < largo; l++)
+	{
+		_putchar(str[l]);
+	}
+	_putchar('\n');
+}
