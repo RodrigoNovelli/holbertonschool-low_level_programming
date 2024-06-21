6f759ae376b800e71296a6d7ff8c81736f16a8cf
Rodrigo Novelli <Rodrigonovelli21.com>
2024-06-21 23:25:18 +0000

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - This function concatenates n bytes of a
+ * string with another one.
+ *@dest: This must be the string that you wanna put first
+ *@src: You will add n bytes from here to dest.
+ *@n: the bytes you wana copy form src
+ * Return: dest pointer
+ *  **/
+char *_strncat(char *dest, char *src, int n)
+{
+	int counter;
+	int rec;
+	char *p;
+
+	counter = 0;
+	while (dest[counter] != 0)
+	{
+		counter++;
+	}
+	p = &dest[counter];
+	for (rec = 0; rec <= n - 1; rec++)
+	{
+		*p = src[rec];
+		p++;
+	}
+	return (dest);
+}
