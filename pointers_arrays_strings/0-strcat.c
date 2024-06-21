571e518854079922a26d32862b75cd4cd8827b5a
Rodrigo Novelli <Rodrigonovelli21.com>
2024-06-21 22:30:01 +0000

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ **_strcat : this function concatenates two strings
+ *@dest: this is the first string
+ *@src: this is the istring you wanna conncatenate with to
+ *return: a pointer to the final string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int counter1;
+	int counter2;
+	int counter3;
+	char *p;
+	
+	counter1 = 0;
+	while (dest[counter1] != 0)
+	{
+		dest[counter1];
+		counter1++;
+	}
+	counter2 = 0;
+	while (src[counter2]  != 0)
+	{
+		src[counter2];
+		counter2++;
+	}
+	counter3 = 0;
+	for (p = &dest[counter1], counter3 <= counter2; p++)
+	{
+		*p = src[counter3];
+		counter3++;
+
+	}
+	return (dest);
+}
