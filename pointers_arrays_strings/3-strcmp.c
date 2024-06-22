7a5b071bae7e7fd561d1e5cef70bcaf03750f74e
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-06-22 13:22:00 +0000

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - compares the lenght of two strings
+ *@s1: the string you wanna know if is greater than s2
+ *@s2: the string you wanna compare s1 to
+ *Return: -15 (its shorter), 15 (its greater) 0 (equal)
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+	int counter1 = 0;
+	int counter2 = 0;
+
+	while (s1[counter1] != 0)
+		counter1++;
+	while (s2[counter2] != 0)
+		counter2++;
+	if (counter1 < counter2)
+		return (-15);
+	else if (counter1 > counter2)
+		return (15);
+	else
+		return (0);
+}
