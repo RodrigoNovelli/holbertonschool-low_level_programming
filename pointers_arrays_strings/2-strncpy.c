3ebd8fba1c9c48a147f6dc3009dc8366db0ea21c
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-06-22 11:57:00 +0000

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strncpy - this function copies a source string to a destination
+ * one
+ * @dest: the array where you wanna copy she source,
+ * @src: the string you want to copy
+ * @n: the amount of ahcaracteres to be copied
+ * Return: a pointer to dest.
+ * **/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int counter1;
+	int counter0;
+
+	counter0 = 0;
+	while (dest[counter0] != 0)
+	{
+		counter0++;
+	}
+	for (counter1 = 0; counter1 <= n; counter1++)
+	{
+		dest[counter1] = src[counter1];
+	}
+	for (counter1 = n + 1; counter1 <= counter0; counter1++)
+	{
+		dest[counter1] = '\0';
+	}
+	return (dest);
+}
