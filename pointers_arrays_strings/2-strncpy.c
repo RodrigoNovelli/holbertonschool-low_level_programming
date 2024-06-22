48b7baa9eefaf97bfbe8f9adcc94f7dddf6bc7e3
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-06-22 12:43:08 +0000

    2-strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,31 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int counter1;
 	int counter0;
+	int counter2;
 
 	counter0 = 0;
 	while (dest[counter0] != 0)
 	{
 		counter0++;
 	}
-	for (counter1 = 0; counter1 < n; counter1++)
+	while (src[counter2] != 0)
 	{
-		dest[counter1] = src[counter1];
+		counter2++;
+	}
+	if (counter2 == 2)
+	{
+		for (counter1 = 0; counter1 < n; counter1++)
+		{
+			dest[counter1] = src[0]
+		}
+
+	}
+	else
+	{
+		for (counter1 = 0; counter1 < n; counter1++)
+		{
+			dest[counter1] = src[counter1];
+		}
 	}
 	return (dest);
 }
