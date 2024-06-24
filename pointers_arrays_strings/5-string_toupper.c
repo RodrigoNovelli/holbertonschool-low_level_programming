58fb9dff765b5fe93a7fc787ed746730a294eab4
RodrigoNovelli <Rodirgonovelli21@hotmail.com>
2024-06-24 13:38:48 +0000

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - this function changes lower for upper cases:
+ * @a: the string you wanna change the lower cases.
+ * Return: pointer to char
+ */
+
+char *string_toupper(char *a)
+{
+	int counter = 0;
+	int x = 0;
+		counter++;
+	for (x = 0; x <= counter; x++)
+	{
+		if (a[x] >= 97 && a[x] <= 122)
+			a[x] = a[x] - 32;
+		else
+			continue;
+	}
+	return (a);
+}
