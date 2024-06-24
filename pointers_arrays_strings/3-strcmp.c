005b789fe4c7b99abce6b8e958341853abad2422
RodrigoNovelli <Rodirgonovelli21@hotmail.com>
2024-06-24 13:01:36 +0000

    3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -15,5 +15,10 @@ int _strcmp(char *s1, char *s2)
 		counter1++;
 	while (s2[counter2] != 0)
 		counter2++;
-        return (counter1 - counter2);
+        if (counter1 < counter2)
+		return (-13);
+	else if (counter1 > counter2)
+		return (13);
+	else
+		return (0);
 }
