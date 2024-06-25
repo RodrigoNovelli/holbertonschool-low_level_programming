aaf6a20704b7965db6a80b4525cb42c316fd2b3d
Rodrigo Novelli <RodrigoNovelli21@hotmail.com>
2024-06-25 14:42:00 +0000

    2-strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - this functions will find the first char of a array
+ * @s: the array you wanna find c
+ * @c: the character you wanna find.
+ * Return: x a oiunter to the char.
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *x;
+	int counter = 0;
+
+	while (s[counter] != 0)
+	{
+		if (s[counter] == c)
+		{
+			x = &s[counter];
+			break;
+		}
+		counter++;
+	}
+	return (x);
+}
