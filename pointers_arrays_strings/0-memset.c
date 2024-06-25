9475a31225d56533b961cfa7ff53780f84f01c7f
Rodrigo Novelli <RodrigoNovelli21@hotmail.com>
2024-06-25 13:23:26 +0000

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - This function fills n spaces of bytes from s pointer
+ * with b.
+ * @s: the pointer you wanna part from
+ * @b: the char you wanna put.
+ * @n: the number of byts in s ypu wanna change.
+ *Return: a pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		*s = b;
+		s++;
+	}
+	return (s);
+}
