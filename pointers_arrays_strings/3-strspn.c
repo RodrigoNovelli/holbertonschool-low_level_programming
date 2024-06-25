b9d09391587c15e041cfd8e73962d7dcfe78543f
Rodrigo Novelli <RodrigoNovelli21@hotmail.com>
2024-06-25 15:11:38 +0000

    3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - Compares two strings and tells you how many similituds
+ * between the two arrays are
+ * @s: the string you wanna recore
+ * @accept: the string you wanna compare
+ * Return: the amount of similitudes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int c = 0;
+	int y = 0;
+	unsigned int x;
+
+	x = 0;
+	while (s[c] != '\0')
+	{
+		while (accept[y] != '\0')
+		{
+			if (s[c] == accept[y])
+			{
+				x = x + 1;
+			}
+			y++;
+		}
+		c++;
+		y = 0;
+	}
+	return (x - 2);
+}
