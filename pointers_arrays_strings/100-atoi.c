7deddb55c070ac860cd9edfc287a4af23ba3fa44
Rodri <rodrigonovelli21>
2024-06-25 22:29:34 +0000

    xd

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -18,30 +18,36 @@ int _atoi(char *s)
 			j = adr;
 			num = s[adr] - 48;
 			adr++;
+			for (adr = 0; adr <= j; adr++)
+			{
+				if (s[adr] == '-')
+					sign = sign * -1;
+				else if (s[adr] == '+')
+					sign = sign * 1;
+			}
+		}
+                else
+                        continue;                                                                                               }
 			while (s[adr] >= '0' && s[adr] <= '9')
 			{
-				num = (num * 10) + (s[adr] - 48);
-				adr++;
+				if ((num * sign)  > '0')
+				{
+					num = (num * 10) + (s[adr] - 48);
+					adr++;
+				}
+				else if ((num * sign) < '0')
+				{
+					num = (num * -10) - (s[adr] - 48);
+					adr++;
+				}
 				if (s[adr] <= '0' || s[adr] >= '9')
 					break;
 			}
 			if (s[adr] <= '0' || s[adr] >= '9')
 				break;
 		}
-		else if (num == -2147483647)
-			num = -214748367 - 1;
 		else
 			adr++;
 	}
-	for (adr = 0; adr <= j; adr++)
-	{
-		if (s[adr] == '-')
-			sign = sign * -1;
-		else if (s[adr] == '+')
-			sign = sign * 1;
-		else
-			continue;
-	}
-	num = num * sign;
 	return (num);
 }
