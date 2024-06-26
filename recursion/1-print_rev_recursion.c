8e72b3dc1b9366638084935b178c4499e33a361a
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-06-26 16:27:41 +0000

    1 without null

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _print_rev_recursion - Prints all the string
+ * _print_rev_recursion - Prints all the string in reverse.
  * @s: the string you wanna print.
  *Return: void
  */
@@ -9,7 +9,6 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != 0)
 	{
-		
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
