92934cde55f54bc2ef26c640b392972d9756e155
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-06-27 14:56:01 +0000

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * division - dividies a number
+ * @x: the number you will divide
+ * @y: you divide x between y
+ * @lim: increments x until lim
+ * Return: how many divisors it have
+ */
 int division(int x, int y, int lim)
 {
 	if ((x % y == 0 && x <= lim) || x < 0 || x == 1)
@@ -7,7 +13,7 @@ int division(int x, int y, int lim)
 		return (0);
 	}
 	else if (x % y != 0 && x < lim)
-	{ 
+	{
 		return (division(x, y + 1, lim));
 	}
 	else
@@ -15,7 +21,13 @@ int division(int x, int y, int lim)
 		return (1);
 	}
 }
+
+/**
+ * is_prime_number - knows if a number is prime
+ *@n: the number you wanna know about
+ *Return: Always 1 is prime, 0 its not
+ */
 int is_prime_number(int n)
 {
-	       return (division(n, 2, n));
+	return (division(n, 2, n));
 }
