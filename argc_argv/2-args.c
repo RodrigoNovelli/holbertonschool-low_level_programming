2fac1bbee6b1fd88ed19dbc0133b269c3263ebd3
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-06-28 14:00:12 +0000

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this function will print the number of arguments
+ * @argc: the number of argumetns
+ * @argv: array pointers to array of arguments.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int n;
+
+	(void) argv;
+	for (n = 1; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+	return (0);
+}
