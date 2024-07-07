5ce5771f6343889520bca33f74cb284e1110c809
rodrigo <rodrigonovelli>
2024-07-07 23:10:17 +0000

    4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -6,10 +6,9 @@ void free_grid(int **grid, int height)
 	int n = 0;
 	int y = 0;
 
-	for (grid[height] > 0)
+	for (grid[height]; height > 0; height--)
 	{
 		free(grid[height]);
-		height--;
 	}
 	free(grid);
 }
