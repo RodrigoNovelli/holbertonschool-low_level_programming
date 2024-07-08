bd08a6a09bc5ddf412a6c525f4b12a54d830dee1
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-08 14:14:11 +0000

    1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - frees the memory that a grid made with malloc have
+ *@grid: the grid
+ *@height: the height of the grid
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int x, y, z;
+	char *p;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	x = 0;
+	while (s1[x] != 0)
+		x++;
+	y = 0;
+	while (s2[y] != 0)
+		y++;
+	if (n >= y)
+		n = y;
+	p = malloc((x + n) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (z = 0; z <= x + n; z++)
+	{
+		p[z] = s1[z];
+		if (z >= x)
+		{
+			for(y = 0; y <= n; y++)
+			{
+				p[z] = s2[y];
+				z++;
+			}
+		}
+	}
+	return (p);
+}
