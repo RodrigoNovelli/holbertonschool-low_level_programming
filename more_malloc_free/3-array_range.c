647d58dc8d2d701e556c929a98712eeaf68ad83f
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-08 17:35:49 +0000

    3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings in a malloc space of memory
+ *@s1: the first string
+ *@s2: the second string
+ * Return: a pointer, or NULL if something goes wrong
+ */
+int *array_range(int min, int max)
+{
+	int *p;
+	int counter;
+	int size;
+
+	size = max - min;
+	if (min > max)
+		return (NULL);
+	p = malloc((size + 1) * sizeof(int));
+	if (p == NULL)
+		return (NULL);
+	for (counter = 0; counter < size; counter++)
+	{
+		p[counter] = min;
+		min++;
+	}
+	return (p);
+}
