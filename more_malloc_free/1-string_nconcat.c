4a23389cb7ae004eaf0b9b68fc0e810dbec7cf74
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-08 14:29:24 +0000

    n menos 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		y++;
 	if (n >= y)
 		n = y;
-	p = malloc((x + n + 1) * sizeof(char));
+	p = malloc((x + n) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
 	for (z = 0; z <= x + n; z++)
