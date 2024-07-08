49366190a586dc7dd2bb7a0c8637f4f3e7f2bb3d
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-08 16:53:49 +0000

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings in a malloc space of memory
+ *@s1: the first string
+ *@s2: the second string
+ * Return: a pointer, or NULL if something goes wrong
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	char *zero;
+	unsigned int counter;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	zero = p;
+	for(counter = 0; counter <= (nmemb * size); counter++)
+	{
+		zero[counter] = 0;
+	}
+	return (p);
+}
