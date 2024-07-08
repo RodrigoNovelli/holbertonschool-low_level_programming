811ad1887f4ea35cfb116c410d45a53178f39d92
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-08 13:26:31 +0000

    4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,5 +1,19 @@
 #include <stdlib.h>
 #include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 /**
  * free_grid - frees the memory that a grid made with malloc have
  *@grid: the grid
@@ -11,9 +25,17 @@ void free_grid(int **grid, int height)
 {
 	int n;
 
-	for (n = 0; n <= height; n++)
+	if (grid == NULL)
 	{
-		free(grid[n]);
+		_putchar('O');
+		_putchar('K');
 	}
-	free(grid);
+	else
+	{
+		for (n = 0; n <= height; n++)
+		{
+			free(grid[n]);
+		}
+		free(grid);
+	}
 }
