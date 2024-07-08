060d1398bab26f0603722bc79deafd3d990f41de
RodrigoNovelli <Rodrigonovelli21@homtail.com>
2024-07-08 23:29:04 +0000

    Define is used to declare a macro, and the diff structures must be between ()

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
-/*
+#ifndef DOG_H
+#define DOG_H (dog)
+/**
  * struct dog - Stores the data of dogs
  * @name: The name of the dog as a string
  * @age: The age of the dog as a float
- * @owner: The owner of the dog in a string*/
+ * @owner: The owner of the dog in a string
+ *
+ * Description: in this structure you can store the data of your actual dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
