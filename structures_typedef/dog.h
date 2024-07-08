f0ed263813cb955663c30b284b9c71b7af00696f
RodrigoNovelli <Rodrigonovelli21@homtail.com>
2024-07-08 23:11:39 +0000

    dog.h, my first structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,11 @@
+/*
+ * struct dog - Stores the data of dogs
+ * @name: The name of the dog as a string
+ * @age: The age of the dog as a float
+ * @owner: The owner of the dog in a string*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
