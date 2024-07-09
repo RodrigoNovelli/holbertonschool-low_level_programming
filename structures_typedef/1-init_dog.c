92eb2c62614c415ac1465d782e861058e5618ae9
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-09 13:23:36 +0000

    Function that changes the values of dog structure

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "dog.h"
-/*
+/**
+ * init_dog - This funtion changes the values in dog stucture.
+ * @d: pointer to memory where dog structure is stored.
+ * @name: will be the value od name in the structure
+ * @age: will be the value of age in the structure
+ * @owner: will be the value of the owner in the structure.
+ * Return: void
  * */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
