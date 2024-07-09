b7af01b53a1acf88dc9b89e75d36b77ce70837bd
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-09 18:13:06 +0000

    Free the dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - frees the dog space
+ * @d: the pointer to the array of data
+ * Return : void function
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
