b84fdde4c34069a91992b92f97c5dd2129e84b32
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-09 13:33:28 +0000

    Putting an exit for null d

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -7,11 +7,11 @@
  * @age: will be the value of age in the structure
  * @owner: will be the value of the owner in the structure.
  * Return: void
- * */
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		exit (1)
+		exit(1);
 	else
 	{
 		d->name = name;
