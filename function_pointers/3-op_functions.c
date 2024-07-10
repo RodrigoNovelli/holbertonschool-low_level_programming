c2a5688eab0bdfc6b3fa0e6f866da7104ff5e750
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-10 23:51:59 +0000

    op functions

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - adds two ints
+ * @a: first addend
+ * @b: second addend
+ * Return: the total of a plus b, int type.
+ */
+int op_add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+	return (r);
+}
+/**
+ * op_sub - substracts two ints
+ * @a: the minuend
+ * @b: the subtrahend
+ * Return: the difference between a and b, int data type
+ */
+int op_sub(int a, int b)
+{
+	int r;
+
+	r = a - r;
+	return (r);
+}
+/**
+ * op_mul - multiplies two ints
+ * @a: the first factor
+ * @b: the second factor
+ * Return: The product between a and b (int).
+ */
+int op_mul(int a, int b)
+{
+	int r;
+
+	r = a * r;
+	return (r);
+}
+/**
+ * op_div - divides two numbers
+ * @a: the dvisor
+ * @b: the dividend
+ * Return: quotient between a and b, int type.
+ */
+int op_div(int a, int b)
+{
+	int r;
+
+	r = a / r;
+	return (r);
+}
+/**
+ * op_mod - function that returns the remainder between
+ * two ints.
+ *   */
+int op_mod(int a, int b)
+{
+	int r;
+
+	r = a % b;
+	return (r);
+}
