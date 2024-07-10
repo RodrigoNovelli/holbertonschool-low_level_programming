ab46616649e07de2b3e6c08d54c35dc62eafed5d
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-10 13:14:28 +0000

    solving betty problems

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- *
+ * print_name - This function recibes a variable name and recalls a function,
+ * it puts the name variable as the pointed function variable.
+ * @name: The name you wanna print.
+ * @f: A pointer to a function that exist.
+ * Return : Its void, it will only print, dont return any value.
  */
 void print_name(char *name, void (*f)(char *))
 {
