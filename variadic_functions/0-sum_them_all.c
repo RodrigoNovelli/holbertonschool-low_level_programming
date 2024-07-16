02eff40a325b2708a24bc5923c96b48ef7e3dc17
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-16 18:51:04 +0000

    The first exercise of variadic functions

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	va_start(sumandos, n);
-	for (counter = 0; counter <= n; counter++)
+	for (counter = 0; counter < n; counter++)
 	{
 		res = res + va_arg(sumandos, int);
 	}
