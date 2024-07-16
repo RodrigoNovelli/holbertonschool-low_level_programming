4df3674c322c24b948619a603dab604c416e1449
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-07-16 18:44:10 +0000

    The first exercise of variadic functions

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - this is a function that sums an
+ * indeterminated amount of numbers.
+ * @n: the total amount of numbers.
+ * Return: the total.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int counter;
+	va_list sumandos;
+	unsigned int res = 0;
+
+	va_sart(sumandos, n);
+	for (counter = 0; counter <= n; counter++)
+	{
+		res = res + va_list(sumandos, n);
+	}
+	va_end(sumandos);
+	return (res);
+}
---
+++ variadic_functions/README.md
@@ -0,0 +1,1 @@
+This is abut variadic functions 
