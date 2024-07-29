4400abaab03c5d95d569eb3cbb3d863a6b7c44c8
Rodrigo Novelli <RodrigoNovelli21@hotmail.com>
2024-07-29 17:14:44 +0000

    lets see now

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -6,16 +6,11 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t n;
+	size_t n = 0;
 	const list_t *current = h;
 
 	while (current != NULL)
 	{
-		if (current->next == h)
-		{
-			n++;
-			return (n);
-		}
 		n++;
 		current = current->next;
 	}
