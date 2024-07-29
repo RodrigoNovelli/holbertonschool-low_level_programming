6d450589b0ccccd60d5fb033ce423ec9d4916e89
Rodrigo Novelli <RodrigoNovelli21@hotmail.com>
2024-07-29 16:26:20 +0000

    Solving 1 element problem

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -9,10 +9,12 @@ size_t list_len(const list_t *h)
 	size_t n;
 	const list_t *current = h;
 
-	while (current != 0)
+	while (current != NULL)
 	{
 		current = current->next;
 		n++;
+		if (current->next == NULL)
+			break;
 	}
 	return (n);
 
