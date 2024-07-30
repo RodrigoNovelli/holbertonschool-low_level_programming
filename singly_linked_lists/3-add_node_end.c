0e95b2d6cdd1cd1a50fcf5ea20408df1f0978124
Rodrigo Novelli <RodrigoNovelli21@hotmail.com>
2024-07-30 14:47:36 +0000

    solving segmetation fault

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,34 +1,40 @@
 #include "lists.h"
+/**
+ * add_node_end - Changes the tail of the linked list.
+ * @str: This is the string, must be duplicated;
+ * @head: a pointer that points the pointer of the head
+ * Return: A pointer to the new tail.
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newtail  = *head;
-	char *p;
+	list_t *current;
+	list_t *newtail;
 	int i = 0;
 
+	newtail = malloc(sizeof(list_t));
+	if (newtail == NULL)
+	{
+		free(newtail);
+		return (NULL);
+	}
+	newtail->str = strdup(str);
+	while (str[i] != 0)
+		i++;
+	if (newtail->str == NULL)
+	{
+		free(newtail);
+		return (NULL);
+	}
+	newtail->len = i;
+	newtail->next = NULL;
 	if (*head == NULL)
 	{
-		*head = malloc(sizeof(list_t));
+		*head = newtail;
+		return (newtail);
 	}
-	p = strdup(str);
-	while (p[i] != 0)
-		i++;
-	while (newtail->next != 0)
-	{
-		newtail = newtail->next;
-	}
-	newtail->next = malloc(sizeof(list_t));
-	if (newtail == NULL)
-        {
-                free(newtail);
-                return (NULL);
-        }
-	newtail = newtail->next;
-	newtail->str = p;
-        if (p == NULL)
-        {
-                free(newtail);
-                return (NULL);
-        }
-	newtail->next = NULL;
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = newtail;
 	return (newtail);
 }
