16b1ad2932c982d8f8a9ded99812fd37dc55c529
RodrigoNovelli21@hotmail.com <RodrigoNovelli21@hotmail.com>
2024-08-03 14:42:49 +0000

    2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * add_dnodeint - Changes the head of a doubly linked list
+ * @head: head of the linked list (if its null it creates a new one)
+ * @n: value to add to the list
+ * Return: pointer to the head.
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newhead;
@@ -7,13 +13,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (newhead == NULL)
 		return (NULL);
 	newhead->n = n;
+	if (*head == NULL)
+	{
+		*head = newhead;
+		newhead->next = NULL;
+		newhead->prev = NULL;
+		return (newhead);
+	}
 	newhead->next = *head;
 	newhead->prev = NULL;
 	(*head)->prev = newhead;
-	if (head == NULL)
-	{
-		*head = newhead;
-		return (newhead);
-	}
+	*head = newhead;
 	return (newhead);
 }
