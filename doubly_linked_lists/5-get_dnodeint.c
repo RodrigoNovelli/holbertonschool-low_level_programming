9667272632d03e6f64ae426f5b6ff5e838ace436
RodrigoNovelli21@hotmail.com <RodrigoNovelli21@hotmail.com>
2024-08-03 15:29:28 +0000

    index problems

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current;
 	unsigned int counter = 0;
-	unsigned int check = 0;
 
 	current = head;
 	for (counter = 0; counter < index; counter++)
