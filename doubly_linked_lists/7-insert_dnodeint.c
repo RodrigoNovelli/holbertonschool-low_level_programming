a8912c51126a524189565db5c4f0cc6a7c8192e5
RodrigoNovelli21@hotmail.com <RodrigoNovelli21@hotmail.com>
2024-08-03 16:28:49 +0000

    if index is 1

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
         if (new == NULL)
                 return (NULL);
-	if (idx  == 0)
+	if (idx  == 0 || *h == NULL)
 	{
 		*h = new;
 		new->next = current;
